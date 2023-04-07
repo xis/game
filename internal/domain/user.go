a8d939abb701eb7425cde2b0c589c13059c1c921
xis_xi <fopeak@gmail.com>
2023-04-07 12:52:28 +0300

    feat(user.go): add CheckExistsByID method to UserRepository interface

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -12,6 +12,7 @@ type User struct {
 type UserRepository interface {
 	Create(ctx context.Context, username, password string) (User, error)
 	GetByName(ctx context.Context, username string) (User, error)
+	CheckExistsByID(ctx context.Context, id string) (bool, error)
 	CheckExistsByName(ctx context.Context, username string) (bool, error)
 	GetUsersByIDs(ctx context.Context, ids []string) ([]User, error)
 }
