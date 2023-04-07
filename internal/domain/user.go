d74674ecb6df27cccc8b5afa2d2ff4343e5e73e2
xis_xi <fopeak@gmail.com>
2023-04-07 11:54:19 +0300

    feat(user.go): add User struct and UserRepository interface with methods for user creation, retrieval, and existence checking

---
+++ internal/domain/user.go
@@ -0,0 +1,17 @@
+package domain
+
+import "context"
+
+type User struct {
+	ID           string
+	Name         string
+	PasswordHash string
+}
+
+//go:generate mockery --name UserRepository --structname MockUserRepository --outpkg mocks --filename user_repository_mock.go --output ./mocks/. --with-expecter
+type UserRepository interface {
+	Create(ctx context.Context, username, password string) (User, error)
+	GetByName(ctx context.Context, username string) (User, error)
+	CheckExistsByName(ctx context.Context, username string) (bool, error)
+	GetUsersByIDs(ctx context.Context, ids []string) ([]User, error)
+}
